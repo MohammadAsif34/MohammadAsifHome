3e7a8322d03d04524235626f7ada153f993e5eab
MohammadAsif34 <asifhasnain134@gmail.com>
2024-09-26 22:28:02 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Mohammad Asif Projeact</h1>
+      <h1>
+        Mohammad Asif Project <i className="bi bi-droplet"></i>
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {/* <Navbar /> */}
     </>
   );
 }
